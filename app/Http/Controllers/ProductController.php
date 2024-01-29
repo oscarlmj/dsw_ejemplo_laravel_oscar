8dc5a0f8934afd002548542428bdc940fb71a559
Oscar Moreno <oscarlmorenojorge@gmail.com>
2024-01-29 08:29:15 +0000

    Subida

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+
+    public function index() {
+        $viewData = [];
+        $viewData["title"] = "Página principal - Tienda online";
+
+        return view("home.index")->with("viewData", $viewData);
+    }
+
+    // Controlador de la página "Acerca de"
+    public function about() {
+        $viewData = [];
+        $viewData["title"] = "Acerca de - Tienda Online";
+        $viewData["subtitle"] =  "Acerca de";
+        $viewData["description"] =  "Esta es la página acerca de ...";
+        $viewData["author"] = "Desarrollado por: DSW";
+
+        return view("home.about")->with("viewData", $viewData);
+    }
+
+    public function products(){
+        $viewData = [];
+
+        $viewData["title"] = "Lista de productos";
+        $viewData["subtitle"] =  "Listado de productos";
+        $viewData["description"] =  "Este es el listado de productos";
+        $viewData["author"] = "Desarrollado por: DSW";
+
+        return view("home.products")->with("viewData", $viewData);
+    }
+}
