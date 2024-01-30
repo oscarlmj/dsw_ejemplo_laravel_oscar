db20b6a218cae168f1ec98084a80850de7441711
Oscar Moreno <oscarlmorenojorge@gmail.com>
2024-01-30 13:05:36 +0000

    Cambios en el controlador de productos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,43 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-<<<<<<< HEAD
 
-use Illuminate\Http\Request;
-
-class HomeController extends Controller
-{
-
-    public function index() {
-        $viewData = [];
-        $viewData["title"] = "Página principal - Tienda online";
-
-        return view("home.index")->with("viewData", $viewData);
-    }
-
-    // Controlador de la página "Acerca de"
-    public function about() {
-        $viewData = [];
-        $viewData["title"] = "Acerca de - Tienda Online";
-        $viewData["subtitle"] =  "Acerca de";
-        $viewData["description"] =  "Esta es la página acerca de ...";
-        $viewData["author"] = "Desarrollado por: DSW";
-
-        return view("home.about")->with("viewData", $viewData);
-    }
-
-    public function products(){
-        $viewData = [];
-
-        $viewData["title"] = "Lista de productos";
-        $viewData["subtitle"] =  "Listado de productos";
-        $viewData["description"] =  "Este es el listado de productos";
-        $viewData["author"] = "Desarrollado por: DSW";
-
-        return view("home.products")->with("viewData", $viewData);
-    }
-}
-=======
 use App\Models\Producto;
 use Illuminate\HTTP\Request;
 
@@ -64,4 +28,3 @@ class ProductController extends Controller
         return view('product.show')->with("viewData", $viewData);
     }
 }
->>>>>>> 8b0a46797a34f8ba03618962800db0daaaa6c5b7
