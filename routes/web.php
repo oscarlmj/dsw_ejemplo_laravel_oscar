682edc2125b42f865f31d85e688ae026928127e4
Oscar Moreno <oscarlmorenojorge@gmail.com>
2024-01-30 13:06:08 +0000

    Cambios en las rutas

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,9 @@ Route::get('/', "App\Http\Controllers\HomeController@index")->name("home.index")
 
 Route::get("/about", "App\Http\Controllers\HomeController@about")->name("home.about");
 
-<<<<<<< HEAD
-Route::get("/products","App\Http\Controllers\ProductsController@index")->name("home.products");
 
-Route::get('/productos/{id}', [App\Http\Controllers\ProductsController::class, 'show'])->name('products.show');
-
-=======
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
->>>>>>> 8b0a46797a34f8ba03618962800db0daaaa6c5b7
+
+Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
+Route::get('/admin/products', 'App\Http\Controllers\Admin\AdminProductController@index')->name("admin.product.index");
